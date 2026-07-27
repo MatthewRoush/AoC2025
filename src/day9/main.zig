39c4aa2ce5c6c7ec7443e7fda13509ac76d580f3
Matthew Roush <matthew.roush@proton.me>
2026-07-27 15:26:23 -0400

    Day 9 puzzle 2.
    Completed day 9 puzzle 2. It was very easy once I realized the solution (seven months later.)

--- src/day9/main.zig
+++ src/day9/main.zig
@@ -25,8 +25,6 @@ pub fn main() void {
 }
 
 fn solve(allocator: std.mem.Allocator, input: []const u8, comptime puzzle: utils.Puzzle) u64 {
-    if (puzzle == .puzzle2) return 0;
-
     var tiles: std.ArrayList(Vector2) = .empty;
     defer tiles.deinit(allocator);
 
@@ -42,13 +40,58 @@ fn solve(allocator: std.mem.Allocator, input: []const u8, comptime puzzle: utils
             .y = std.fmt.parseInt(i32, line[comma + 1 ..], 10) catch unreachable,
         };
 
-        for (tiles.items) |other_tile| {
-            const new_rect = rectArea(tile, other_tile);
-
-            if (new_rect > biggest_rect) biggest_rect = new_rect;
-        }
-
         tiles.append(allocator, tile) catch unreachable;
+    }
+
+    for (tiles.items, 0..) |this_tile, i| {
+        for (i..tiles.items.len + 1) |_k| {
+            const k = (_k + 1) % tiles.items.len;
+            const other_tile = tiles.items[k];
+
+            switch (puzzle) {
+                .puzzle1 => {
+                    const new_rect = rectArea(this_tile, other_tile);
+                    biggest_rect = @max(biggest_rect, new_rect);
+                },
+                .puzzle2 => {
+                    const new_rect = rectArea(this_tile, other_tile);
+
+                    const rect_min_x = @min(this_tile.x, other_tile.x);
+                    const rect_max_x = @max(this_tile.x, other_tile.x);
+                    const rect_min_y = @min(this_tile.y, other_tile.y);
+                    const rect_max_y = @max(this_tile.y, other_tile.y);
+
+                    if (new_rect > biggest_rect) {
+                        var is_within_lines = true;
+
+                        for (tiles.items, 0..) |edge_p1, j| {
+                            const edge_p2 = tiles.items[(j + 1) % tiles.items.len];
+
+                            const edge_min_x = @min(edge_p1.x, edge_p2.x);
+                            const edge_max_x = @max(edge_p1.x, edge_p2.x);
+                            const edge_min_y = @min(edge_p1.y, edge_p2.y);
+                            const edge_max_y = @max(edge_p1.y, edge_p2.y);
+
+                            if (rect_min_x > edge_max_x or
+                                rect_max_x < edge_min_x or
+                                rect_min_y > edge_max_y or
+                                rect_max_y < edge_min_y) continue;
+
+                            const horizontal_edge = edge_min_y == edge_max_y;
+                            const vertical_edge   = edge_min_x == edge_max_x;
+
+                            if (horizontal_edge and (edge_min_y == this_tile.y or edge_min_y == other_tile.y)) continue;
+                            if (vertical_edge   and (edge_min_x == this_tile.x or edge_min_x == other_tile.x)) continue;
+
+                            is_within_lines = false;
+                            break;
+                        }
+
+                        if (is_within_lines) biggest_rect = new_rect;
+                    }
+                },
+            }
+        }
     }
 
     return biggest_rect;
